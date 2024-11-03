ced2997b36361ce834b9717b81071bc1720abf69
Aleix Soler <aleixsoler64@gmail.com>
2024-11-03 13:48:54 +0100

    chore: improve the text and speed of the displayed message

--- worker.gd
+++ worker.gd
@@ -2,13 +2,12 @@ extends Node2D
 
 @onready var status_label: Label = $Label # Nodo Label para mostrar el estado
 
-const GOOD_WORKER_SAYS = "a circle?? in the triangle factory?? how queer!! ive never seen such a thing- i must inquire about this further with my supervisor post-hastel!!"
+const GOOD_WORKER_SAYS = "a circle?? in the triangle factory?? how queer!!\nive never seen such a thing- i must inquire about\n this further with my supervisor post-hastel!!"
 const BAD_WORKER_SAYS = "i guess we doing circles now"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -29,17 +28,15 @@ func should_notify_supervisor() -> bool:
 
 # Acción de notificar al supervisor
 func notify_supervisor():
-	print("Notificando al supervisor: ¡pieza inesperada detectada!")
 	update_status_label(GOOD_WORKER_SAYS)
 
 # Acción de ignorar la pieza inesperada
 func ignore_unexpected_piece():
-	print("Ignorando la pieza inesperada.")
 	update_status_label(BAD_WORKER_SAYS)
 	
 	# Actualiza el texto del label y lo muestra por un breve periodo
 func update_status_label(message: String) -> void:
 	status_label.text = message
 	status_label.visible = true
-	await get_tree().create_timer(1.5).timeout # Muestra el mensaje por 1.5 segundos
+	await get_tree().create_timer(3).timeout # Muestra el mensaje por 3 segundos
 	status_label.visible = false
